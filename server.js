d8599f89d8f429ed1f8ffd1d8f205bb386b99a71
TaniuOp <taniuop@gmail.com>
2021-11-16 15:20:20 +0100

    ejercicio del clima con Node.js + Express + OpenWeather

--- server.js
+++ server.js
@@ -1,22 +1,40 @@
 const express = require('express')
 const app = express()
 let ejs = require('ejs');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'); //para usar el req.body 
 app.use(bodyParser.urlencoded({ extended: true }));
+let apiKey = '2c173e3ef608ab30dd56e81fb45abfc5';
+let request = require('request');
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('index')
-})
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  res.render('index', {weather: null, error: null});
 })
 
 app.post('/', function (req, res) {
-    res.render('index');
-    console.log(req.body.city);
+    // res.render('index');
+    // console.log(req.body.city);
+    let city = req.body.city;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+ 
+    request(url, function (err, response, body) {
+      if(err){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weather = JSON.parse(body)
+        if(weather.main == undefined){
+          res.render('index', {weather: null, error: 'Error, please try again'});
+        } else {
+          let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+          res.render('index', {weather: weatherText, error: null});
+        }
+      }
+    });
   })
 
+  app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+  })
+  
